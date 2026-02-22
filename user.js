8869a7f27330229e1ac6666391fd62a1415004d6
yashvardhanshrivastava-hub <yashvardhanshrivastava42@gmail.com>
2026-02-23 04:12:03 +0530

    Delete user.js

--- user.js
+++
@@ -1,9 +0,0 @@
-router.post("/", async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.status(201).json({ message: "User saved" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
